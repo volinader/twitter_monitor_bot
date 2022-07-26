f4f0eb431d90a4b449486612a68e65007a141af4
Volinader <onki29@yandex.ru>
2022-07-27 00:55:31 +0300

    delete my secret keys

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 
 #Settings for api
-consumer_key = "9KZH9HxU0I0ciQNaj91ljomH0"
-consumer_secret = "lSHnlH8D5gFnAemAQytepHoEwYvHXzxKEh7XopXKfWpwXUYb8j"
-access_token = "133836093-S8IntTWwijenxGmM6QkkYY7RWfNSSbQSEKRIl11N"
-access_token_secret = "87XYJhjnULweXtXtJo3O2QmmYJQDDZaI07BJG7FlBPvAN"
-bearer_token ="AAAAAAAAAAAAAAAAAAAAAFLBcgEAAAAAbarZTE3mbzjMuzjgU08tA2Dl7O0%3Dha1eJpD9O0bFuHK5joYRk8dDOGXbSeI9tklRDJfgggcsFF6bVv"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+bearer_token =""
 
 
 #Settings for webhoock
 
-url='https://discord.com/api/webhooks/984954432519282688/Z1SPnLXnrsHi6S1SJiaMAbIC6qjwxyvvASFOBfPSJSDcEtcePslOJgmb_0B8lZ27-dsx'
+url=''
